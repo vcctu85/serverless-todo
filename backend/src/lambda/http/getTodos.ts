5c198343ac23e5801edae9a1e64041f5898897ee
vitu <vitu@cisco.com>
2020-04-19 19:59:28 -0700

    getTodo

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,7 +1,38 @@
 import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import * as AWS  from 'aws-sdk'
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+const todoTable = process.env.TODO_TABLE
+const userTable = process.env.USER_TABLE
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   // TODO: Get all TODO items for a current user
+
+  const userId = event.pathParameters.userId
+  const todo_items = await getTODOPerUser(userId)
+  return {
+    statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      items: todo_items
+    })
+  }
+  
+}
+
+async function getTODOPerUser(groupId: string) {
+  const result = await docClient.query({
+    TableName: todoTable,
+    KeyConditionExpression: 'groupId = :groupId',
+    ExpressionAttributeValues: {
+      ':groupId': groupId
+    },
+    ScanIndexForward: false
+  }).promise()
+
+  return result.Items
 }
