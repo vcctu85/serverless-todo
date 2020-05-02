f61474cd173765ac9207a0c57b5b4040240e5b1c
vitu <vitu@cisco.com>
2020-05-02 13:31:28 -0700

    × TypeError: Cannot read property 'map' of undefined

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,32 +1,23 @@
 import 'source-map-support/register'
 import { getToken } from '../auth/auth0Authorizer'
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
-import * as AWS  from 'aws-sdk'
+// import * as AWS  from 'aws-sdk'
 import { decode } from 'jsonwebtoken'
-const docClient = new AWS.DynamoDB.DocumentClient()
-const todoTable = process.env.TODO_TABLE
+// const docClient = new AWS.DynamoDB.DocumentClient()
+// const todoTable = process.env.TODO_TABLE
 import { getTODOPerUser } from '../../business-logic/todos-logic'
+import { createLogger } from '../../utils/logger'
 
+
+const logger = createLogger("getTodos")
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   // TODO: Get all TODO items for a current user
   console.log(event)
   const jwtToken = getToken(event.headers['Authorization'])
-
+  logger.info('new todo item', event);
   const userId = decode(jwtToken).sub
   console.log("Decoded userId: ", userId)
-  //const validUserId = await userExists(userId)
-  // if (!validUserId) {
-  //   return {
-  //     statusCode: 404,
-  //     headers: {
-  //       'Access-Control-Allow-Origin': '*',
-  //       'Access-Control-Allow-Credentials': true
-  //     },
-  //     body: JSON.stringify({
-  //       error: 'Group does not exist'
-  //     })
-  //   }
-  // }
+
   const todo_items = await getTODOPerUser(userId)
   console.log(todo_items)
   return {
@@ -35,22 +26,22 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Credentials': true
     },
-    body: userId
+    body: JSON.stringify(event)
   }
   
 }
 
-async function userExists(userId: string) {
-  const result = await docClient
-    .get({
-      TableName: todoTable,
-      Key: {
-        id: userId
-      }
-    })
-    .promise()
+// async function userExists(userId: string) {
+//   const result = await docClient
+//     .get({
+//       TableName: todoTable,
+//       Key: {
+//         id: userId
+//       }
+//     })
+//     .promise()
 
-  console.log('Get user: ', result)
-  return !!result.Item
-}
+//   console.log('Get user: ', result)
+//   return !!result.Item
+// }
 
